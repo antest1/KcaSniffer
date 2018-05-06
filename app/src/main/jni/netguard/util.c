e4df01f05dab217ef53f3848a7778d4f15ee5289
antest1 <alias_n@live.jp>
2018-05-07 03:16:07 +0900

    update gradle script to the latest, switch from gradle experimental to cmake

--- app/src/main/jni/netguard/util.c
+++ app/src/main/jni/netguard/util.c
@@ -61,6 +61,7 @@ int sdk_int(JNIEnv *env) {
 }
 
 void log_android(int prio, const char *fmt, ...) {
+    /*
     if (prio >= loglevel) {
         char line[1024];
         va_list argptr;
@@ -68,7 +69,7 @@ void log_android(int prio, const char *fmt, ...) {
         vsprintf(line, fmt, argptr);
         __android_log_print(prio, TAG, line);
         va_end(argptr);
-    }
+    }*/
 }
 
 uint8_t char2nible(const char c) {
